d6277e72f0a316974ffdb7383fd0683c7a2049cf
PARSAKING77 <parsakingkp@gmail.com>
2024-10-16 17:46:31 +0330

    Read all the title by enter the title number

---
+++ 3_Read_Book.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+BOOKS = [{"title": "Title One", "author": "Author One", "category": "science"},
+    {"title": "Title Two", "author": "Author Two", "category": "science"},
+    {"title": "Title Three", "author": "Author Three", "category": "science"},
+    {"title": "Title Four", "author": "Author Four", "category": "history"},
+    {"title": "Title Five", "author": "Author Five", "category": "science"},
+    {"title": "Title Six", "author": "Author Two", "category": "math"}
+]
+
+@app.get('/book{book_title}')
+def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
