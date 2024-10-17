37630ba204943f1b4128124bf2792e66a040b510
PARSAKING77 <parsakingkp@gmail.com>
2024-10-17 21:07:41 +0330

    this is the first commit of the Books project

---
+++ 10_Books_Project.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    descreption: str
+    rating: int
+
+    def __init__(self, id, title, author, descreption, rating):
+
+        self.id = id
+        self.title = title
+        self.author = author
+        self.descreption = descreption
+        self.rating = rating
+
+
+books = [
+
+    Book(1, 'Shoe Dog', 'Folani', 'How nike makes', 8),
+    Book(2, 'Roobi', 'Folan kas', 'Its about a fox goes to prison', 3),
+    Book(3, 'Shafaei Zendegi', 'Giti', 'Its about soul of human', 9),
+    Book(4, 'You dont know js yet', '...', 'Its about the JavaScrpict', 9)
+]
+
+
+@app.get('/books')
+def read_all_books():
+    return books
