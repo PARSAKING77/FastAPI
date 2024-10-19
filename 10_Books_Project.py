5b8ef33b9a1eefe0574be2d7c798aac67177ebcf
PARSAKING77 <parsakingkp@gmail.com>
2024-10-19 13:45:15 +0330

    in this code wi add the finding the book with the id of the book and when you make a book the id is defualt take the last id in book list

--- 10_Books_Project.py
+++ 10_Books_Project.py
@@ -5,48 +5,32 @@ app = FastAPI()
 
 
 class Book:
-    id: int
-    title: str
-    author: str
-    descreption: str
-    rating: int
-
-    def __init__(self, id, title, author, descreption, rating):
-
+    def __init__(self, id: int, title: str, author: str, description: str, rating: int):
         self.id = id
         self.title = title
         self.author = author
-        self.descreption = descreption
+        self.description = description
         self.rating = rating
 
+
+
 class BookRequest(BaseModel):
-    id: id
+    id: int
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
-    descreption: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_length=1, max_length=100)  # Corrected spelling
     rating: int = Field(gt=0, lt=6)
 
-    class Config:
 
-        schema_extra = {
-
-            'example' : {
-
-                'title' : 'Eneter name of your new book',
-                'author' : 'Enter your name',
-                'descreption' : 'Enter the Descreption of your new book',
-                'rating' : 5
-            }
-        }
 
 
 BOOKS = [
-
-    Book(1, 'Shoe Dog', 'Folani', 'How nike makes', 8),
-    Book(2, 'Roobi', 'Folan kas', 'Its about a fox goes to prison', 3),
-    Book(3, 'Shafaei Zendegi', 'Giti', 'Its about soul of human', 9),
-    Book(4, 'You dont know js yet', '...', 'Its about the JavaScrpict', 9)
+    Book(1, 'Shoe Dog', 'Folani', 'How Nike makes', 8),
+    Book(2, 'Roobi', 'Folan kas', 'It\'s about a fox that goes to prison', 3),
+    Book(3, 'Shafaei Zendegi', 'Giti', 'It\'s about the soul of a human', 9),
+    Book(4, 'You Don\'t Know JS Yet', '...', 'It\'s about JavaScript', 9)
 ]
+
 
 
 @app.get('/books')
@@ -55,19 +39,32 @@ def read_all_books():
 
 
 
-
 @app.post('/create-book')
-def craete_book(book_request: BookRequest):
+def create_book(book_request: BookRequest):
     new_book = Book(**book_request.dict())
-    return books.append(find_book_id(new_book))
+
+    find_book_id(new_book)
+    BOOKS.append(new_book)  
+
+    return new_book
+
 
 
 def find_book_id(book: Book):
     if len(BOOKS) > 0:
         book.id = BOOKS[-1].id + 1
-
     else:
         book.id = 1
-
-
     return book
+
+
+
+@app.get('/books/{book_id}')
+
+def read_book(book_id: int):
+    for book in BOOKS:
+
+        if book.id == book_id:
+            return book
+            
+    return {"error": "Book not found"}
