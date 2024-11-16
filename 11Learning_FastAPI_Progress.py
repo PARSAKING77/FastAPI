a01542cd82972899ccdc45d755477cb45f86a366
PARSAKING77 <parsakingkp@gmail.com>
2024-11-16 23:28:51 +0330

    The query parameter q is of type Union[str, None] (or str | None in Python 3.10), that means that it's of type str but could also be None, and indeed, the default value is None, so FastAPI will know it's not required.

--- 11Learning_FastAPI_Progress.py
+++ 11Learning_FastAPI_Progress.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
 
 app = FastAPI()
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+@app.get("/items/")
+async def read_items(q: str | None = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
