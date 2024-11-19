ac0a57125ce0678b34248f145b802d7f7277e265
PARSAKING77 <parsakingkp@gmail.com>
2024-11-19 16:37:02 +0330

    You can declare examples for a Pydantic model that will be added to the generated JSON Schema.

--- 11Learning_FastAPI_Progress.py
+++ 11Learning_FastAPI_Progress.py
@@ -9,7 +9,19 @@ class Item(BaseModel):
     description: str | None = None
     price: float
     tax: float | None = None
-    tags: list = []
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                }
+            ]
+        }
+    }
 
 
 @app.put("/items/{item_id}")
