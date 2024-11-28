830995c45384d5732d2297e5ba6c71181704ff6d
PARSAKING77 <parsakingkp@gmail.com>
2024-11-28 20:46:09 +0330

    the response class and avoid the extra overhead that FastAPI would have by passing your return content through the jsonable_encoder before passing it to the response class.

--- 11Learning_FastAPI_Progress.py
+++ 11Learning_FastAPI_Progress.py
@@ -1,22 +1,9 @@
-from datetime import datetime
-from typing import Union
-
 from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    title: str
-    timestamp: datetime
-    description: Union[str, None] = None
-
+from fastapi.responses import ORJSONResponse
 
 app = FastAPI()
 
 
-@app.put("/items/{id}")
-def update_item(id: str, item: Item):
-    json_compatible_item_data = jsonable_encoder(item)
-    return JSONResponse(content=json_compatible_item_data)
+@app.get("/items/", response_class=ORJSONResponse)
+async def read_items():
+    return ORJSONResponse([{"item_id": "Foo"}])
