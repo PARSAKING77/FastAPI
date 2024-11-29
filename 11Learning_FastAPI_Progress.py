80ebf95f3d693bb4012cfa1af0fd3779a1c6966e
PARSAKING77 <parsakingkp@gmail.com>
2024-11-29 19:17:14 +0330

    To return a response with HTML directly from FastAPI, use HTMLResponse.

--- 11Learning_FastAPI_Progress.py
+++ 11Learning_FastAPI_Progress.py
@@ -19,4 +19,21 @@ app = FastAPI()
 @app.put("/items/{id}")
 def update_item(id: str, item: Item):
     json_compatible_item_data = jsonable_encoder(item)
-    return JSONResponse(content=json_compatible_item_data)
+    return JSONResponse(content=from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/items/", response_class=HTMLResponse)
+async def read_items():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """)
