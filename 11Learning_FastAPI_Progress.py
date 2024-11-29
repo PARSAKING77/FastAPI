ee3226868620022b67b55dc46b3d02bf7e5a8bcb
PARSAKING77 <parsakingkp@gmail.com>
2024-11-29 19:41:50 +0330

    Even though all your code is written assuming there's just /app.

--- 11Learning_FastAPI_Progress.py
+++ 11Learning_FastAPI_Progress.py
@@ -1,19 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
 @app.get("/app")
-def read_main():
-    return {"message": "Hello World from main app"}
-
-
-subapi = FastAPI()
-
-
-@subapi.get("/sub")
-def read_sub():
-    return {"message": "Hello World from sub API"}
-
-
-app.mount("/subapi", subapi)
+def read_main(request: Request):
+    return {"message": "Hello World", "root_path": request.scope.get("root_path")}
